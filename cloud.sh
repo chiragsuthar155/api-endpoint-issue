431c4e2449866c92f52d447743c51a75d62b3d7c
Himanshu Masih <m331096@one.merckgroup.com>
2024-04-21 05:45:10 +0000

    Update cloud.sh, .gitlab-ci.yml

--- cloud.sh
+++ cloud.sh
@@ -7,15 +7,16 @@ for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27
 do
 
   echo "==============ECOM NSLOOKUP=================="
-  nslookup cloud-analytics.sial.com
-  nslookup repo.maven.apache.org
+  nslookup adm.sigmaaldrich.com
+  #nslookup repo.maven.apache.org
   sleep 1
   
-  echo "==============ECOM CURL======================"
-  curl  -I --cacert ./cloud-analytics.sial.crt  "https://cloud-analytics.sial.com/deep?get=recentlyViewedAPI&guid=GA1.2.1607803001.1571084413&country=US&language=en&product=t1503&brand=sigma&count=10&addProduct=1&disp=recentlyViewedAdvancedSearch"
-  sleep 1
+  # echo "==============ECOM CURL======================"
+  # curl  -I --cacert ./cloud-analytics.sial.crt  "https://cloud-analytics.sial.com/deep?get=recentlyViewedAPI&guid=GA1.2.1607803001.1571084413&country=US&language=en&product=t1503&brand=sigma&count=10&addProduct=1&disp=recentlyViewedAdvancedSearch"
+  # sleep 1
 
   echo "for base url"
-  curl --insecure -I https://cloud-analytics.sial.com
+  #curl --insecure -I https://cloud-analytics.sial.com
+  curl -O  https://adm.sigmaaldrich.com/tms/v2/view/properties?sessionid=gitlab&filename=/repos/node/translations/messages_zh_CN.json
   sleep 1
 done
